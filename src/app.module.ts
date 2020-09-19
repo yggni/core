de3a5c246c7f596b533ed95daaf79018f1254356
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-19 13:09:30 -0300

    fix: deps not working

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { EnigmaModule } from '@yggni/enigma';
+import { } from '@yggni/enigma';
 
 @Module({
   imports: [EnigmaModule],
