ecaef9fe9e89a78358dfb4667b6bb2173237311f
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-19 13:04:36 -0300

    chore: update deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { EnigmaModule } from 'enigma';
+import { EnigmaModule } from '@yggni/enigma';
 
 @Module({
   imports: [EnigmaModule],
