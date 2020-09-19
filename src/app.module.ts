3cbeee9a2080d4a01142e2cc103bdaf859fe3140
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-19 13:18:15 -0300

    fix: erros in deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { } from '@yggni/enigma';
+import { EnigmaModule } from '@yggni/enigma';
 
 @Module({
   imports: [EnigmaModule],
